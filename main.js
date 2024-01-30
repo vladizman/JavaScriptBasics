0314898b8883b5ca3d5d6c4c01c79022fb60895c
vladizman <vladizmanq@gmail.com>
2024-01-30 18:40:30 +0200

    Header part dynamic

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Menu section
+
 document.querySelector("#open-nav-menu").addEventListener("click", function () {
   document.querySelector("header nav .wrapper").classList.add("nav-open")
 })
@@ -7,3 +9,16 @@ document
   .addEventListener("click", function () {
     document.querySelector("header nav .wrapper").classList.remove("nav-open")
   })
+
+//Header part
+
+const greetingText = "Good Night!"
+const weatherCondition = "Sunny"
+const userLocation = "Chisinau"
+let temperature = 22.1241
+let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(
+  1
+)}°C outside.`
+
+document.querySelector("#greeting").innerHTML = greetingText
+document.querySelector("p#weather").innerHTML = weatherText
