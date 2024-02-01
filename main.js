ed37a517b7e8d354b763f0a98daec830c738b403
vladizman <vladizmanq@gmail.com>
2024-02-01 20:47:18 +0200

    converting Celsius to Fahr

--- main.js
+++ main.js
@@ -11,14 +11,32 @@ document
   })
 
 //Header part
+function calculateTemperature(temperature) {
+  let fahr = temperature * 1.8 + 32
+  return fahr
+}
 
 const greetingText = "Good Night!"
 const weatherCondition = "Sunny"
 const userLocation = "Chisinau"
-let temperature = 22.1241
-let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(
+let temperature = 22
+let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(
   1
 )}°C outside.`
 
+let fehrText = `The weather is ${weatherCondition} in ${userLocation} and it's ${calculateTemperature(
+  temperature
+).toFixed(1)}°C outside.`
+
 document.querySelector("#greeting").innerHTML = greetingText
-document.querySelector("p#weather").innerHTML = weatherText
+document.querySelector("p#weather").innerHTML = celsiusText
+
+document
+  .querySelector(".weather-group")
+  .addEventListener("click", function (e) {
+    if (e.target.id == "celsius") {
+      document.querySelector("p#weather").innerHTML = celsiusText
+    } else if (e.target.id == "fahr") {
+      document.querySelector("p#weather").innerHTML = fehrText
+    }
+  })
